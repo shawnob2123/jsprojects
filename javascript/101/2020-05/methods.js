69a4981111f1d24a1cb12fecff6917902150b586
Shawn O'Brien <65410367+shawnob2123@users.noreply.github.com>
2020-05-28 17:20:55 -0400

    Create methods.js

---
+++ javascript/101/2020-05/methods.js
@@ -0,0 +1,26 @@
+// slice
+const array1 = ["A", "B", "C", "D", "E"];
+const numbers = ["1", "2", "3", "4", "5"];
+
+console.log(array1.slice(2));
+console.log(numbers.slice(1));
+
+
+//splice
+const names = ["Shawn", "Harry", "Tom", "Dan"]
+names.splice(4, 2, 'Eddy', 'Steve');
+
+console.log(names);
+
+//forEach
+
+
+//push
+
+
+
+//pop
+
+
+
+//indexOf
